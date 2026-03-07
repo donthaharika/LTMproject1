2c4612b97ff4e5bdd29dc4fe1c50299d5a9682c8
donthaharika <dummy@gmail.com>
2026-03-07 12:08:51 +0000

    day12

--- server/src/main/java/com/edutech/progressive/service/InsuranceService.java
+++ server/src/main/java/com/edutech/progressive/service/InsuranceService.java
@@ -2,18 +2,17 @@ package com.edutech.progressive.service;
 
 import com.edutech.progressive.entity.Insurance;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public interface InsuranceService {
 
-    List<Insurance> getAllInsurances() throws SQLException;
+    List<Insurance> getAllInsurances();
 
-    int addInsurance(Insurance insurance) throws SQLException;
+    int addInsurance(Insurance insurance);
 
-    Insurance getInsuranceById(int insuranceId) throws SQLException;
+    Insurance getInsuranceById(int insuranceId);
 
-    void updateInsurance(Insurance insurance) throws SQLException;
+    void updateInsurance(Insurance insurance);
 
-    void deleteInsurance(int insuranceId) throws SQLException;
+    void deleteInsurance(int insuranceId);
 }
--- server/src/main/java/com/edutech/progressive/service/impl/ProductServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/ProductServiceImplJdbc.java
@@ -1,46 +1,68 @@
 package com.edutech.progressive.service.impl;
- 
+
 import java.sql.SQLException;
 import java.util.List;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
- 
+
 import com.edutech.progressive.dao.ProductDAO;
 import com.edutech.progressive.entity.Product;
 import com.edutech.progressive.service.ProductService;
- 
-public class ProductServiceImplJdbc implements ProductService{
-    @Autowired
-    ProductDAO productDAO;
-    
-    public ProductServiceImplJdbc(ProductDAO productDAO) {
-        this.productDAO = productDAO;
-    }
- 
-    @Override
-    public List<Product> getAllProducts() throws SQLException {
-        return productDAO.getAllProducts();
-    }
- 
-    @Override
-    public Product getProductById(int productId) throws SQLException {
-        return productDAO.getProductById(productId);
-    }
- 
-    @Override
-    public int addProduct(Product product) throws SQLException {
-        return productDAO.addProduct(product);
-    }
- 
-    @Override
-    public void updateProduct(Product product) throws SQLException {
-        productDAO.updateProduct(product);
-    }
- 
-    @Override
-    public void deleteProduct(int productId) throws SQLException {
-        productDAO.deleteProduct(productId);
-    }
- 
- 
-}
+
+// public class ProductServiceImplJdbc implements ProductService  {
+
+//     private ProductDAO productDAO;
+
+//     @Autowired
+// public ProductServiceImplJdbc(ProductDAO productDAO) {
+//         this.productDAO = productDAO;
+//     }
+
+//     public ProductServiceImplJdbc() {
+// }
+
+//     @Override
+//     public List<Product> getAllProducts() {
+//         // TODO Auto-generated method stub
+//         // throw new UnsupportedOperationException("Unimplemented method 'getAllProducts'");
+//         return productDAO.getAllProducts();
+//     }
+
+  
+//     @Override
+//     public Product getProductById(int productId) {
+//         // TODO Auto-generated method stub
+//         // throw new UnsupportedOperationException("Unimplemented method 'getProductById'");
+//         return productDAO.getProductById(productId);
+//     }
+
+//     @Override
+//     public int addProduct(Product product) {
+//         // TODO Auto-generated method stub
+//         // throw new UnsupportedOperationException("Unimplemented method 'addProduct'");
+//         try {
+//             return productDAO.addProduct(product);
+//         } catch (SQLException e) {
+//             // TODO Auto-generated catch block
+//             e.printStackTrace();
+//         }
+//         return -1;
+//     }
+
+//     @Override
+//     public void updateProduct(Product product) {
+//         // TODO Auto-generated method stub
+//         // throw new UnsupportedOperationException("Unimplemented method 'updateProduct'");
+//         productDAO.updateProduct(product);
+        
+        
+//     }
+
+//     @Override
+//     public void deleteProduct(int productId) {
+//         // TODO Auto-generated method stub
+//         // throw new UnsupportedOperationException("Unimplemented method 'deleteProduct'");
+//         productDAO.deleteProduct(productId);
+//     }
+
+// }
